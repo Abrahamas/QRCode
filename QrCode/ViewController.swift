26c3817adffe8b75fd89ad65a4025013dd73180b
Abrahamas <babyyuguioh@gmail.com>
2019-02-08 23:04:13 -0500

    Initial Commit

---
+++ QrCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QrCode
+//
+//  Created by Abrahamas on 11/19/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
